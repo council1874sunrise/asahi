37d3281889be542ef522629c6b80ca1201610d17
council1874sunrise <council.1874sunrise@gmail.com>
2026-01-17 18:10:20 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
 
   // 2. エラーの原因になるパッケージを強制的に変換対象にする
-  transpilePackages: ['undici', 'firebase', '@firebase/auth'],
+  // ★ここに 'react-qr-reader' を追加しました。これが重要です！
+  transpilePackages: ['undici', 'firebase', '@firebase/auth', 'react-qr-reader'],
 
   // 3. スマホ（クライアント）側では、undici を「空っぽ」に置き換えて無効化する
   webpack: (config, { isServer }) => {
