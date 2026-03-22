2c229292cd166aec243169def105bd1e407bf57d
council1874sunrise <council.1874sunrise@gmail.com>
2026-03-22 18:04:14 +0900

    Update page.tsx

--- app/oM1YBTNBy0gEEFaM9yD43u9V7GA20GQlYJ6/monitor/page.tsx
+++ app/oM1YBTNBy0gEEFaM9yD43u9V7GA20GQlYJ6/monitor/page.tsx
@@ -796,7 +796,7 @@ function CalledCard({
       {/* 大ID */}
       <div
         style={{
-          fontSize: 72,
+          fontSize: 144,
           fontWeight: 900,
           lineHeight: 1,
           letterSpacing: "0.04em",
